e2bb548db4bb8f4c5186bce9941cb870ec83755e
Gabox <gabriel.l.mateu@gmail.com>
2022-09-12 16:24:39 -0300

    objetos

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-function listadoproductos() {
+class Producto{
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    mostrarnombreyprecio(){
+        return`${this.nombre}-> Precio: $$(this.precio)` /* Muestra el nombre y el precio de un objeto de la clase producto */
+    }
+}
+/* Objetos a comercializar */
+const zapatilladanzaclasica = new Producto("Zapatilla danza clasica", 1450);
+const zapatodetap = new Producto("Zapato de Tap", 1950);
+const metatarsiana = new Producto("Metatarsiana", 1150);
+const remeraalgodon = new Producto("Remera de Algodon PLIÉ CHASSÉ JETÉ", 750);
+
+/* Array de carrito vacio */
+
+const carritocompras [];
+
+
+/* funcion que simula una compra */
+
+function mostrarProductos(){
+    let productos=prompt(`Cual de los siguientes productos deseas comprar? \n 1.${zapatilladanzaclasica.mostrarnombreyprecio()} \n 2.${zapatodetap.mostrarnombreyprecio()} \n 3.${metatarsiana.mostrarnombreyprecio()} \n 4.${remeraalgodon.mostrarnombreyprecio()}`);
+
+        switch(productos){
+            case"1":
+                function agregaralcarrito(){
+                    if(carritocompras)
+                }
+        }
+
+
+
+}
+
+
+/* function listadoproductos() {
     let listado = Number(prompt('Productos y precios \n1) Zapatillas danza clasica - $1450 \n2) Zapatos de Tap - $1950 \n3) Metatarsiana - $1150 \n4) Remera de Algodon PLIÉ CHASSÉ JETÉ - $750'))
 
     switch(listado){
@@ -19,3 +56,4 @@ function listadoproductos() {
 
 listadoproductos();
 
+ */
